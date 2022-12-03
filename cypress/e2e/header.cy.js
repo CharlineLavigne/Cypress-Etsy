0273a88df54a7c528646887694f5465109246b13
CharlineLavigne <charline.lavigne5@gmail.com>
2022-12-03 18:30:31 -0500

    add tests of header and its selectors

--- cypress/e2e/header.cy.js
+++ cypress/e2e/header.cy.js
@@ -1,3 +1,41 @@
+import header from '../selectors/header.sel'
+
 describe('Header', () => {
-    
+    beforeEach(() => {
+        cy.visit('')
+    })
+
+    it('can search for products suggested in the search box', () => {
+        cy.get(header.searchBox)
+            .type('music')
+        
+        cy.get(header.suggestionBox)
+            .find('li')
+            .contains('poster')
+            .click() 
+    })
+
+
+    it('can search for Home Decor products from the nav bar', () => {
+        cy.get(header.navBar)
+            .find('a')
+            .eq(3)
+            .trigger('mouseover')
+
+        cy.get(header.decorNav)
+            .click()
+    })
+
+
+    it('can change the currency of the website', () => {
+        cy.get(header.updateSettingsBtn)
+            .click()
+        
+        cy.get(header.currencyCbo)
+            .select('USD')
+            .select('CAD')
+
+        cy.get(header.updateSettingsSaveBtn)
+            .click()
+    })
 })
--- cypress/selectors/header.sel.js
+++ cypress/selectors/header.sel.js
@@ -1,5 +1,11 @@
 module.exports = {
-    
+    searchBox: '#global-enhancements-search-query',
+    suggestionBox: '#global-enhancements-search-suggestions',
+    navBar: 'ul[data-ui="top-nav-category-list"]',
+    decorNav: '#catnav-l4-10962',
+    updateSettingsBtn: '#header-locale-picker-trigger',
+    currencyCbo: '#locale-overlay-select-currency_code',
+    updateSettingsSaveBtn: '#locale-overlay-save'
 }
 
 //search for an element in the search bar, select second suggestion and press enter from keyboard
